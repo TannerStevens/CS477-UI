5949f4723a96616a45ff9cd0d18c5320d3e78fe6
TannerStevens <tanner.stevens42@gmail.com>
2017-04-06 11:44:41 -0700

    MapScreen Update

--- src/MapScreen.js
+++ src/MapScreen.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import Hamburger from './Hamburger.js';
+
 class MapScreen extends React.Component {
     render(){
         return(
-            <p>Map</p>
+            <div style={styles.container}>
+                <Hamburger/>
+                <div style={styles.speed}/>
+            </div>
         );
+    }
+}
+const styles = {
+    container:{
+        display:'flex',
+        flexDirection: 'row',
+        flex: 1,
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: "#d9ead3"
+    },
+    speed:{
+        position:'absolute',
+        bottom:'0',
+        width:'17vw',
+        height:'17vw',
+        borderRadius:'50%',
+        backgroundColor: "#93c47d"
     }
 }
 
